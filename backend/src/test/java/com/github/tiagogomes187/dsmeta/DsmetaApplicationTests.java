84480432ffd8e2e3457939b4bd35b963455c70df
Tiago Gomes <tiagogomes187@gmail.com>
2022-07-11 21:05:19 -0300

    Project created

---
+++ backend/src/test/java/com/github/tiagogomes187/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tiagogomes187.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
