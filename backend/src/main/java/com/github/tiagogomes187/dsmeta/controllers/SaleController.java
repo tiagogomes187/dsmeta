b9c347d4bf442471acd7c10eafa10f01b7b3cbcd
Tiago Gomes <tiagogomes187@gmail.com>
2022-07-13 22:21:32 -0300

    Date select

--- backend/src/main/java/com/github/tiagogomes187/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/github/tiagogomes187/dsmeta/controllers/SaleController.java
@@ -3,8 +3,11 @@ package com.github.tiagogomes187.dsmeta.controllers;
 import com.github.tiagogomes187.dsmeta.entities.Sale;
 import com.github.tiagogomes187.dsmeta.services.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,7 +23,10 @@ public class SaleController {
     }
 
     @GetMapping
-    public List<Sale> findSales(){
-        return services.findSales();
+    public Page<Sale> findSales(
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+            Pageable pageable){
+        return services.findSales(minDate, maxDate, pageable);
     }
 }
--- backend/src/main/java/com/github/tiagogomes187/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/github/tiagogomes187/dsmeta/services/SaleService.java
@@ -3,8 +3,13 @@ package com.github.tiagogomes187.dsmeta.services;
 import com.github.tiagogomes187.dsmeta.entities.Sale;
 import com.github.tiagogomes187.dsmeta.repositories.SaleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.List;
 
 @Service
@@ -16,7 +21,14 @@ public class SaleService {
         this.repository = repository;
     }
 
-    public List<Sale> findSales() {
-        return repository.findAll();
+    public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+
+        LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+        LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+        LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+
+
+        return repository.findSales(min, max, pageable);
     }
 }
